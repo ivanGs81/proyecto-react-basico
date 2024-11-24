9b3d8adff90552b232cee6e8b384875929ee8481
Ivan G <guardia_ivan@icloud.com>
2024-11-24 17:53:50 -0500

    se agrega un nuevo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react"
 import { useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
 function App() {
   const [criptos, setCriptos] = useState()
   const API_URL = import.meta.env.VITE_API_URL;
   useEffect(() => {
-    fetch(`${API_URL}assets`)
-      .then((resp) => resp.json())
+    // fetch(`${API_URL}assets`)
+    axios.get(`${API_URL}assets`)
+      //.then((resp) => resp.json())
       .then((data) => {
-        setCriptos(data.data)
-        //console.log(data.data)
+        setCriptos(data.data.data)
+        console.log(data)
       })
       .catch(() => {
         console.error("la peticion fallo")
@@ -25,7 +28,7 @@ function App() {
       <ul>
         { 
           criptos.map(({id, name, priceUsd}) => (
-            <li key={id}>Nombre: {name}, Precio: {priceUsd}</li>
+            <li key={id} className="verde">Nombre: {name}, Precio: {Math.round(priceUsd)} USD</li>
           )) 
         }
         
