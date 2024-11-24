96879bc39ba4e806b127aec8e1b9f2750b3f6237
Ivan G <guardia_ivan@icloud.com>
2024-11-24 17:05:29 -0500

    conexion a api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react"
+import { useEffect } from "react"
 
 function App() {
+  const [criptos, setCriptos] = useState()
+  const API_URL = import.meta.env.VITE_API_URL;
+  useEffect(() => {
+    fetch(`${API_URL}assets`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCriptos(data.data)
+        //console.log(data.data)
+      })
+      .catch(() => {
+        console.error("la peticion fallo")
+      })
+  }, [])
+
+  if (!criptos) return <span>Cargando...</span>
+
+
   return (
     <>
-      <h1>mi aplicacion limpia</h1>
+      <h1>Lista de criptos</h1>
+      <ul>
+        { 
+          criptos.map(({id, name, priceUsd}) => (
+            <li key={id}>Nombre: {name}, Precio: {priceUsd}</li>
+          )) 
+        }
+        
+      </ul>
     </>
+    
   )
 }
 
