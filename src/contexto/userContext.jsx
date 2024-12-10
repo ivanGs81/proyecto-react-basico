25cc42abd6bc95158ef1456300b4d259dd73a028
Ivan G <guardia_ivan@icloud.com>
2024-12-10 15:50:37 -0500

    arreglos del perfil

--- src/contexto/userContext.jsx
+++ src/contexto/userContext.jsx
@@ -1,16 +1,26 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
+import axios from "axios"
 
 const UserContext = createContext()
 
+
 function UserContextProvider({ children }) {
    const [usering, setUsering] = useState({})
+
    useEffect(() => {
+      axios.post(`https://reqres.in/api/users/2`)
+         .then((data) => {
+            //setProfile(data)
+            console.log(data.data)
+         })
+         .catch(e => console.error(e))
       setUsering({
          name: "Juanito",
          registered: "12/10/2024",
          role: "Administrador"
       })
+      
    }, [])
    return (
       <UserContext.Provider value={usering}>
